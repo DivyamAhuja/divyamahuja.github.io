97e5c3ee165d3f1d4de92ba7205cd636eacab1fb
Divyam Ahuja <ahujadivyam@gmail.com>
2022-06-07 12:16:26 +0530

    Try Fixing Github Pages _next issue

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProd ? '/divyamahuja.github.io/' : '',
 }
 
 module.exports = nextConfig
